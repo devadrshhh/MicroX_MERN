de65b4c74606e3392845c62d00ced044441e6589
microxlearn <learn.microx@gmail.com>
2026-05-14 01:44:27 +0530

    fixed cloudinary public access

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -4,6 +4,14 @@ const cloudinary = require('../config/cloudinary');
 
 const storage = new CloudinaryStorage({
   cloudinary,
+  params: async (req, file) => ({
+    folder: 'microx_pdfs',
+    resource_type: 'raw',
+    type: 'upload',
+    public_id: `${Date.now()}-${file.originalname.replace(/\s+/g, '-')}`,
+  }),
+});
+  cloudinary,
   params: {
     folder: 'microx_pdfs',
     resource_type: 'raw',
