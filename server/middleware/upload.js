4963aeef70557f989b545ab0c8fb156c33306c23
microxlearn <learn.microx@gmail.com>
2026-05-14 01:50:37 +0530

    fixed cloudinary public access

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -11,13 +11,6 @@ const storage = new CloudinaryStorage({
     public_id: `${Date.now()}-${file.originalname.replace(/\s+/g, '-')}`,
   }),
 });
-  cloudinary,
-  params: {
-    folder: 'microx_pdfs',
-    resource_type: 'raw',
-    public_id: (req, file) => Date.now() + '-' + file.originalname,
-  },
-});
 
 const upload = multer({ storage });
 
