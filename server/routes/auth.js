9f625455f56e89aee34fb93be97920bb704e0935
microxlearn <learn.microx@gmail.com>
2026-05-13 23:48:19 +0530

    fixed register validation

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,7 +8,9 @@ const Admin = require('../models/Admin');
 // Register
 router.post('/register', async (req, res) => {
   try {
-    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+  return res.status(400).json({ message: 'All fields required' });
+}
     const normalizedEmail = email.toLowerCase().trim();
     
     const existingUser = await User.findOne({ email: normalizedEmail });
